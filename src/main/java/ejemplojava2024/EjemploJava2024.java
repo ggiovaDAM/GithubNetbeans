590578b12a1eb2064c8b4e95f0dd5d6dcbe2c24e
Guido <guido.giovadam@gmail.com>
2024-02-04 16:01:54 +0100

    Primer commit del trabajo de Github en Netbeans, incluyendo la estructura del proyecto

---
+++ src/main/java/ejemplojava2024/EjemploJava2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejemplojava2024;
+
+/**
+ *
+ * @author Guido
+ */
+public class EjemploJava2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, don Pepito");
+    }
+}
